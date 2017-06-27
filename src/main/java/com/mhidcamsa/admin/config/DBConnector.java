e8ade63b2a67bca51150ea398bb6c0fa1eb0263d
dfmontano <dalx2549@gmail.com>
2017-06-26 23:42:33 -0500

    Add DB Connection

--- src/main/java/com/mhidcamsa/admin/config/DBConnector.java
+++ src/main/java/com/mhidcamsa/admin/config/DBConnector.java
@@ -1,4 +1,21 @@
 package com.mhidcamsa.admin.config;
 
+import org.javalite.activejdbc.Base;
+
+
 public class DBConnector {
+
+    public void openConnection(){
+
+        Base.open("com.mysql.cj.jdbc.Driver", "jdbc:mysql://localhost:3306/mhidcam_db", "root", "");
+
+    }
+
+    public void closeConnection(){
+
+        Base.close();
+
+    }
+
+
 }
